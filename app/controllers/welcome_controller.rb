f374b62c7f196635b3eccf0723161e102a4e1d55
Alik Lynn Lyle <aliklynnlyle@gmail.com>
2019-06-04 15:57:45 -0400

    separated rider and admin logins

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,18 @@
 class WelcomeController < ApplicationController
+  layout false
+  layout 'application', :only => :index
+  layout 'rider_layout', :only => :rider
+  layout false, :only => :welcome
+  
+  def welcome
+  
+  end
+
+  def rider
+
+  end
+
   def index
+
   end
 end
