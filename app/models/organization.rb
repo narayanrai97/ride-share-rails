9daee4ccf9e3ed90541f62bd4318f44483f9874a
Tonyynot14 <Wadant3@gmail.com>
2019-06-11 11:20:41 -0400

    Removed Organization

--- app/models/organization.rb
+++
@@ -1,9 +0,0 @@
-class Organization < ApplicationRecord
-  has_many :drivers, dependent: :destroy
-  has_many :riders, dependent: :destroy
-  has_many :rides, dependent: :destroy
-
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
-end
