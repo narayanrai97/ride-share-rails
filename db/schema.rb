ca62670799a8e944780ba160e3baaa8f8dd687ce
Tonyynot14 <Wadant3@gmail.com>
2019-06-12 09:05:18 -0400

    Removed merge conflict tags from schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2019_06_11_152517) do
-=======
-ActiveRecord::Schema.define(version: 2019_06_07_171649) do
->>>>>>> master
+
+
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
