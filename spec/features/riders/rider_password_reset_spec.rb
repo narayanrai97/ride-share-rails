bf686e33e21f236a5025428ce34907ccc97ade9a
Narayan Rai <narayanrai97@gmail.com>
2019-06-19 10:06:53 -0400

    Add rider ability to reset password with test coverage

---
+++ spec/features/riders/rider_password_reset_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.feature "Riders::RiderPasswordResets", type: :feature do
+  let!(:rider) { create :rider }
+
+  scenario "sends a password reset link if a valid email is provided" do
+    visit new_rider_session_path
+    click_link "Forgot your password?"
+    expect(page).to have_selector('h2', text: 'Forgot your password?')
+    fill_in "Email", :with => rider.email
+    click_button "Send me reset password instructions"
+    expect(current_path).to eql(new_rider_session_path)
+    expect(page).to have_text "Log in"
+  end
+
+  scenario "error occurs if an invalid email is provided" do
+    visit new_rider_session_path
+    click_link "Forgot your password?"
+    expect(page).to have_selector('h2', text: 'Forgot your password?')
+    fill_in "Email", :with => 'invalid_rider@example.com'
+    click_button "Send me reset password instructions"
+    expect(current_path).to eql(rider_password_path)
+    expect(page).to have_text "Email not found"
+  end
+end
