d4b5a20f8933d3c2512d9369a91e8ba403ba6cba
Tonyynot14 <Wadant3@gmail.com>
2019-06-19 13:35:46 -0400

    Changed serializer to output object type at begginning of json

--- app/serializers/vehicle_serializer.rb
+++ app/serializers/vehicle_serializer.rb
@@ -1,5 +1,6 @@
 class VehicleSerializer < ActiveModel::Serializer
-
+#Puts root on json element sent
+ActiveModelSerializers.config.adapter = :json_api
   attributes :id,:driver_id, :car_make, :car_model, :car_year,:car_color,
               :car_plate, :seat_belt_num, :insurance_provider, :insurance_start, :insurance_stop
   belongs_to :driver
--- spec/controllers/api/vehicles_controller_spec.rb
+++ spec/controllers/api/vehicles_controller_spec.rb
@@ -4,35 +4,55 @@ RSpec.describe Api::VehiclesController, type: :request do
     let!(:organization) { FactoryBot.create(:organization) }
     #Created a token to by pass login but had to include
     #token_created_at in the last day so it would function
-    let!(:driver) { FactoryBot.create(:driver, organization_id: 2,
+    let!(:driver) { FactoryBot.create(:driver, organization_id: organization.id,
        auth_token: "1234",token_created_at: Time.zone.now) }
+    let!(:vehicle){FactoryBot.create(:vehicle, driver_id: driver.id)}
+    let!(:vehicle1){FactoryBot.create(:vehicle, driver_id: driver.id)}
+
+
+
     it 'will create a vehicle with logged in user' do
 
-    post '/api/v1/vehicles', headers: {"ACCEPT" => "application/json",  "Token" => "1234"},
-     params: {vehicle: {car_make: "Chevorlet",
-      car_model: "Impala",
-      car_year: 2010,
-      car_color: "Silver",
-      car_plate: "VZW1212",
-      insurance_provider: "Geico",
-      insurance_start: "2019/3/11",
-      insurance_stop: "2020/3/11",
-      seat_belt_num: 5}
-      }
+      post '/api/v1/vehicles', headers: {"ACCEPT" => "application/json",  "Token" => "1234"},
+       params: {vehicle: {car_make: "Chevorlet",
+        car_model: "Impala",
+        car_year: 2010,
+        car_color: "Silver",
+        car_plate: "VZW1212",
+        insurance_provider: "Geico",
+        insurance_start: "2019/3/11",
+        insurance_stop: "2020/3/11",
+        seat_belt_num: 5}
+        }
 
-      expect(response).to have_http_status(201)
-      parsed_json = JSON.parse(response.body)
-      expect(parsed_json['vehicle']['car_make']).to eq('Chevorlet')
-      expect(parsed_json['vehicle']['car_year']).to eq(2010)
-      expect(parsed_json['vehicle']['car_color']).to eq('Silver')
-      expect(parsed_json['vehicle']['car_plate']).to eq('VZW1212')
-      expect(parsed_json['vehicle']['insurance_provider']).to eq('Geico')
-      #Dates a formatted when they are accepted so appear slightly different than above
-      expect(parsed_json['vehicle']['insurance_start']).to eq('2019-03-11')
-      expect(parsed_json['vehicle']['insurance_stop']).to eq('2020-03-11')
-      expect(parsed_json['vehicle']['seat_belt_num']).to eq(5)
+
+        expect(response).to have_http_status(201)
+        parsed_json = JSON.parse(response.body)
+        puts parsed_json
+        expect(parsed_json['vehicle']['car_make']).to eq('Chevorlet')
+        expect(parsed_json['vehicle']['car_year']).to eq(2010)
+        expect(parsed_json['vehicle']['car_color']).to eq('Silver')
+        expect(parsed_json['vehicle']['car_plate']).to eq('VZW1212')
+        expect(parsed_json['vehicle']['insurance_provider']).to eq('Geico')
+        #Dates a formatted when they are accepted so appear slightly different than above
+        expect(parsed_json['vehicle']['insurance_start']).to eq('2019-03-11')
+        expect(parsed_json['vehicle']['insurance_stop']).to eq('2020-03-11')
+        expect(parsed_json['vehicle']['seat_belt_num']).to eq(5)
 
 
     end
+
+    it 'will generate all vehicles of user' do
+
+      get '/api/v1/vehicles', headers: {"ACCEPT" => "application/json",  "Token" => "1234"}
+
+        expect(response).to have_http_status(200)
+        puts response.body
+
+
+    end
+
+
+
 
 end
