5210dbc388051bbb9a533af4fbff15c3e77a3958
KWasi Yeboah <yeboah02208@gmail.com>
2019-06-20 14:40:37 +0000

    added reiver enroll

--- spec/requests/drivers_controller_spec.rb
+++ spec/requests/drivers_controller_spec.rb
@@ -2,12 +2,19 @@ require 'rails_helper'
 
 RSpec.describe Api::DriversController, type: :request do
     let!(:organization) { FactoryBot.create(:organization) }
-    let!(:driver) { FactoryBot.create(:driver, organization_id: 1) }
+    let!(:driver) { FactoryBot.create(:driver, organization_id: organization.id) }
     it 'driver login in' do
     post '/api/v1/login', headers: {"ACCEPT" => "application/json" }, params: { email: "v1@sample.com", password: "password" }
        
        expect(response).to have_http_status(201) 
         puts response.body
     end
-
+    it "enrolls a driver" do 
+       post '/api/v1/drivers', headers: {"ACCEPT" => "application/json"}, params: {driver: { email: "sample@sample.com", password: "password",
+       first_name: "Bob", last_name: "Steve", phone: "3361234567", organization_id: organization.id, radius: 50, is_active: true
+       }}
+       expect(response).to have_http_status(200)
+       parsed_json = JSON.parse(response.body)
+       puts parsed_json
+    end
 end
