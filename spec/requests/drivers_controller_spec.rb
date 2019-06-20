13ffe11345a01f63ad9e40b032ab39a57a4037d0
Tonyynot14 <Wadant3@gmail.com>
2019-06-20 10:21:40 -0400

    Change back fix for drivers_controller

--- spec/requests/drivers_controller_spec.rb
+++ spec/requests/drivers_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Api::DriversController, type: :request do
     let!(:organization) { FactoryBot.create(:organization) }
-    let!(:driver) { FactoryBot.create(:driver, organization_id: organization.id) }
+    let!(:driver) { FactoryBot.create(:driver, organization_id: 1) }
     it 'driver login in' do
     post '/api/v1/login', headers: {"ACCEPT" => "application/json" }, params: { email: "v1@sample.com", password: "password" }
 
