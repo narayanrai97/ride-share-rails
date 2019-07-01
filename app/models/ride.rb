b5fab93ff1901650a38e7622629562d83e52e867
Narayan Rai <narayanrai97@gmail.com>
2019-07-01 17:01:19 +0000

    Add Ride model validations

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -6,7 +6,11 @@ class Ride < ApplicationRecord
   belongs_to :end_location, :class_name => "Location"
   has_one :token
 
-  validates :rider_id, presence:true
+  validates :pick_up_time, :reason, :status, presence: true
+  validates :organization, :driver, :rider, :start_location, :end_location, presence: true
+  validates :status, inclusion: { in: %w(requested pending approved scheduled matched completed),
+            message: "%{value} is not a valid status" }
+ 
 
   # has_many :locations
 
