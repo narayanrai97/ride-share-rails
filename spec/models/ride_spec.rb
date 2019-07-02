24d4b3bd5bda74ea8b427018c55ed13e63a17a5d
Narayan Rai <narayanrai97@gmail.com>
2019-07-02 19:16:45 +0000

    Add Ride model validation tests

---
+++ spec/models/ride_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe Ride, type: :model do
+
+  describe 'Validations' do
+    it { should validate_presence_of(:pick_up_time) }
+    it { should validate_presence_of(:reason) }
+    it { should validate_presence_of(:status) }
+  end
+
+   describe "Associations" do
+    it { should belong_to(:organization) }
+    it { should belong_to(:driver) }
+    it { should belong_to(:rider) }
+    it { should belong_to(:start_location) }
+    it { should belong_to(:end_location) }
+    it { should have_one(:token) }
+  end
+
+end
