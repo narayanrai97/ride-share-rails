8730609fe046f0c03c3d95754586ede2f4ade6d0
Alik Lynn Lyle <aliklynnlyle@gmail.com>
2019-07-02 16:29:58 -0400

    field population and update method fixes

--- app/controllers/admin_ride_controller.rb
+++ app/controllers/admin_ride_controller.rb
@@ -45,7 +45,7 @@ class AdminRideController < ApplicationController
   
         if @ride.save
           @ride.reload
-          redirect_to "/admin_ride/" + @ride.id.to_s
+          redirect_to admin_ride_path(@ride)
         else
           render 'new'
         end
@@ -56,20 +56,48 @@ class AdminRideController < ApplicationController
       end
   
       def update
-        @ride = Ride.find(params[:id])
+
+          @ride = Ride.find(params[:id])
+          @start_location = @ride.start_location
+          @end_location = @ride.end_location
   
-        if @ride.update(ride_params)
-          redirect_to @ride
-        else
-          render 'edit'
-        end
+          if !@start_location.update(
+            street: ride_params[:start_street], 
+            city: ride_params[:start_city],
+            state: ride_params[:start_state],
+            zip: ride_params[:start_zip])
+
+            render 'edit' and return
+          end
+  
+          if !@end_location.update(
+            street: ride_params[:end_street], 
+            city: ride_params[:end_city],
+            state: ride_params[:end_state],
+            zip: ride_params[:end_zip])
+
+            render 'edit' and return
+          end
+  
+          if @ride.update(
+            organization_id: current_user.organization_id,
+            rider_id: ride_params[:rider_id],
+            pick_up_time: ride_params[:pick_up_time],
+            reason: ride_params[:reason])
+            redirect_to admin_ride_path(@ride)
+          else
+            render 'edit'# and return
+            # Another example: render :show, layout: 'top_story'
+          end
+
       end
   
       def destroy
         @ride = Ride.find(params[:id])
         @ride.destroy
   
-        redirect_to rides_path
+        redirect_to admin_ride_index_path
+        # redirect_to rides_path
       end
   
       private
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -8,6 +8,77 @@ class Ride < ApplicationRecord
 
   validates :rider_id, presence:true
 
-  # has_many :locations
+  def start_street
+    if self.start_location
+      self.start_location.street
+    else
+      nil
+    end
+  end
+
+  def end_street
+    if self.end_location
+      self.end_location.street
+    else
+      nil
+    end
+  end
+
+  def start_city
+    if self.end_location
+      self.end_location.city
+    else
+      nil
+    end
+  end
+
+  def start_state
+    if self.start_location
+      self.start_location.state
+    else
+      nil
+    end
+  end
+
+  def start_zip
+    if self.start_location
+      self.start_location.zip
+    else
+      nil
+    end
+  end
+
+  def start_zip
+    if self.start_location
+      self.start_location.zip
+    else
+      nil
+    end
+  end
+
+  def end_city
+    if self.end_location
+      self.end_location.city
+    else
+      nil
+    end
+  end
+
+  def end_state
+    if self.end_location
+      self.end_location.state
+    else
+      nil
+    end
+  end
+
+  def end_zip
+    if self.end_location
+      self.end_location.zip
+    else
+      nil
+    end
+  end
 
 end
+
