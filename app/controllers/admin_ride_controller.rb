9ee7ca9f67ec939d0e2b4d5143b07ffbd1765ab8
Narayan Rai <narayanrai97@gmail.com>
2019-07-04 04:48:26 +0000

    Add tokens flow on ride create by admin

--- app/controllers/admin_ride_controller.rb
+++ app/controllers/admin_ride_controller.rb
@@ -2,83 +2,92 @@ class AdminRideController < ApplicationController
 
     before_action :authenticate_user!
     layout 'administration'
-  
+
       def new
         @ride = Ride.new
       end
-  
+
       def show
         @ride = Ride.find(params[:id])
         @start_location = Location.find(@ride.start_location_id)
         @end_location = Location.find(@ride.end_location_id)
       end
-  
+
       def index
         @rides = Ride.all
       end
-  
+
       def create
-        @start_location = Location.new(
-          street: ride_params[:start_street], 
-          city: ride_params[:start_city],
-          state: ride_params[:start_state],
-          zip: ride_params[:start_zip])
-        @start_location.save
-        @start_location.reload
-  
-        @end_location = Location.new(
-          street: ride_params[:end_street], 
-          city: ride_params[:end_city],
-          state: ride_params[:end_state],
-          zip: ride_params[:end_zip])
-        @end_location.save
-        @end_location.reload
-  
-        @ride = Ride.new(
-          organization_id: current_user.organization_id,
-          rider_id: ride_params[:rider_id], 
-          pick_up_time: ride_params[:pick_up_time],
-          start_location_id: @start_location.id,
-          end_location_id: @end_location.id,
-          reason: ride_params[:reason],
-          status: "requested")
-  
-        if @ride.save
-          @ride.reload
-          redirect_to admin_ride_path(@ride)
+        select_rider = Rider.find(ride_params[:rider_id])
+        @token = select_rider.next_valid_token
+        unless @token.nil?
+          @start_location = Location.new(
+            street: ride_params[:start_street],
+            city: ride_params[:start_city],
+            state: ride_params[:start_state],
+            zip: ride_params[:start_zip])
+          @start_location.save
+          # @start_location.reload
+
+          @end_location = Location.new(
+            street: ride_params[:end_street],
+            city: ride_params[:end_city],
+            state: ride_params[:end_state],
+            zip: ride_params[:end_zip])
+          @end_location.save
+          # @end_location.reload
+
+          @ride = Ride.new(
+            organization_id: current_user.organization_id,
+            rider_id: ride_params[:rider_id],
+            pick_up_time: ride_params[:pick_up_time],
+            start_location_id: @start_location.id,
+            end_location_id: @end_location.id,
+            reason: ride_params[:reason],
+            status: "requested")
+
+          if @ride.save
+            @token.ride_id = @ride.id
+            @token.save
+            flash[:notice] = "Ride created for #{select_rider.full_name}"
+            redirect_to admin_ride_path(@ride)
+          else
+            render 'new'
+          end
         else
-          render 'new'
+          flash[:notice] = "The rider does not have enough valid tokens to request this ride."
+          redirect_to admin_ride_index_path
         end
       end
-  
+
       def edit
         @ride = Ride.find(params[:id])
       end
-  
+
       def update
 
           @ride = Ride.find(params[:id])
           @start_location = @ride.start_location
           @end_location = @ride.end_location
-  
+
           if !@start_location.update(
-            street: ride_params[:start_street], 
+            street: ride_params[:start_street],
             city: ride_params[:start_city],
             state: ride_params[:start_state],
             zip: ride_params[:start_zip])
 
             render 'edit' and return
           end
-  
+
           if !@end_location.update(
-            street: ride_params[:end_street], 
+            street: ride_params[:end_street],
             city: ride_params[:end_city],
             state: ride_params[:end_state],
             zip: ride_params[:end_zip])
 
             render 'edit' and return
           end
-  
+
           if @ride.update(
             organization_id: current_user.organization_id,
             rider_id: ride_params[:rider_id],
@@ -91,23 +100,22 @@ class AdminRideController < ApplicationController
           end
 
       end
-  
+
       def destroy
         @ride = Ride.find(params[:id])
         @ride.destroy
-  
+
         redirect_to admin_ride_index_path
         # redirect_to rides_path
       end
-  
+
       private
       def ride_params
-        params.require(:ride).permit(:rider_id, :driver_id, :pick_up_time, 
-        :start_street, :start_city, :start_state, :start_zip, 
+        params.require(:ride).permit(:rider_id, :driver_id, :pick_up_time,
+        :start_street, :start_city, :start_state, :start_zip,
         :end_street, :end_city, :end_state, :end_zip, :reason, :status)
       end
-  
+
   end
-  
-  
-  
+
+
