54bf5a5d01d2fbefd794389bdc40432636581e83
Alik Lynn Lyle <aliklynnlyle@gmail.com>
2019-07-18 17:23:09 -0400

    working on security for ride belongs to organization

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,6 +1,7 @@
 class RidesController < ApplicationController
 
   before_action :authenticate_rider!
+  before_action :authenticate_user!, :authorize_belongs_to_org!, only: [:show, :update, :edit, :delete]
   layout 'rider_layout'
 
     def new
@@ -108,5 +109,11 @@ class RidesController < ApplicationController
       :start_street, :start_city, :start_state, :start_zip,
       :end_street, :end_city, :end_state, :end_zip, :reason, :status)
     end
+
+    def authorize_belongs_to_org!
+      byebug
+      authorize Ride
+    end
+
 
 end
--- app/policies/ride_policy.rb
+++ app/policies/ride_policy.rb
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
-# controls who can access disposal controller actions
+# limits admin access to admin only in Ride's organization
 class RidePolicy < ApplicationPolicy
-    def ride_or_up?
-
-    end
+    def belongs_to_org?
+      byebug
+      # make sure user is an admin
+      # current_ride.organization == current_user.organization ?
+      ride.organization == current_user.organization
+   end
 
     %i(show? edit? update? delete?).each do |ali|
-
-       alias_method ali, :ride_or_up?  
+      byebug
+      alias_method ali, :belongs_to_org?  
     end
   end
