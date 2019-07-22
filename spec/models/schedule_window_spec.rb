fe169ec58cbde5e2f6058f8b52bb0c0838824341
Narayan Rai <narayanrai97@gmail.com>
2019-07-22 19:16:00 +0000

    Add instance method tests on ScheduleWindow validations

--- spec/models/schedule_window_spec.rb
+++ spec/models/schedule_window_spec.rb
@@ -1,9 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe ScheduleWindow, type: :model do
-    # it "has valid factory" do
-    #     FactoryBot.create(:schedule_window).should be_valid
-    # end
     let(:recurring_schedule_window) { build(:schedule_window, is_recurring: true, start_date: "") }
     let(:recurring_schedule_window2) { build(:schedule_window, is_recurring: true, end_date: "") }
 
@@ -24,5 +21,47 @@ RSpec.describe ScheduleWindow, type: :model do
       it { should belong_to(:driver) }
       it { should have_one(:recurring_pattern) }
       it { should belong_to(:location) }
+    end
+
+    describe 'Cannot be in the past' do
+      it "should validate that start time cannot be in the past" do
+        subject.start_time = "2019-07-01"
+        subject.valid?  # run validations
+        expect(subject.errors[:start_time]).to include('cannot be in the past')
+      end
+
+      it "should validate that end time cannot be in the past" do
+        subject.end_time = "2019-07-01"
+        subject.valid?
+        expect(subject.errors[:end_time]).to include('cannot be in the past')
+      end
+
+      it "should validate that start date cannot be in the past" do
+        subject.start_date = "2019-07-01"
+        subject.valid?  # run validations
+        expect(subject.errors[:start_date]).to include('cannot be in the past')
+      end
+
+      it "should validate that end date cannot be in the past" do
+        subject.end_date = "2019-07-01"
+        subject.valid?
+        expect(subject.errors[:end_date]).to include('cannot be in the past')
+      end
+    end
+
+    describe 'Cannot be overlapped' do
+      it "should validate that start time and end time cannot be overlapped" do
+        subject.start_time = "2019-07-01 9:00am"
+        subject.end_time = "2019-07-01 9:00am"
+        subject.valid?  # run validations
+        expect(subject.valid?).to be_falsey
+      end
+
+      it "should validate that start date and end date cannot be overlapped" do
+        subject.start_time = "2019-07-01"
+        subject.end_time = "2019-07-01"
+        subject.valid?  # run validations
+        expect(subject.valid?).to be_falsey
+      end
     end
 end
