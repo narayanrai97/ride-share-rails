ed19d3cdf83c070296ec8ac05e120967ca44300a
Narayan Rai <narayanrai97@gmail.com>
2019-07-22 16:20:28 +0000

    Add ScheduleWindow validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_27_153256) do
+ActiveRecord::Schema.define(version: 2019_07_22_142348) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -135,7 +135,7 @@ ActiveRecord::Schema.define(version: 2019_06_27_153256) do
     t.datetime "start_time"
     t.datetime "end_time"
     t.bigint "location_id"
-    t.boolean "is_recurring"
+    t.boolean "is_recurring", default: false, null: false
     t.index ["driver_id"], name: "index_schedule_windows_on_driver_id"
     t.index ["location_id"], name: "index_schedule_windows_on_location_id"
   end
