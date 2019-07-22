a11af2d87c6be5dd591e4ce99fa4e70ee9ee4b7e
Narayan Rai <narayanrai97@gmail.com>
2019-07-22 16:33:31 +0000

    Add tests for ScehduleWindow model validations

--- spec/factories/schedule_windows.rb
+++ spec/factories/schedule_windows.rb
@@ -2,9 +2,10 @@ FactoryBot.define do
   factory :schedule_window do
     start_time { Date.today }
     end_time { Date.today + 8.hours }
+    start_date { "2019-07-27" }
+    end_date { "2019-07-30" }
 
     driver
     location
-    organization
   end
 end
--- spec/models/schedule_window_spec.rb
+++ spec/models/schedule_window_spec.rb
@@ -4,11 +4,20 @@ RSpec.describe ScheduleWindow, type: :model do
     # it "has valid factory" do
     #     FactoryBot.create(:schedule_window).should be_valid
     # end
+    let(:recurring_schedule_window) { build(:schedule_window, is_recurring: true, start_date: "") }
+    let(:recurring_schedule_window2) { build(:schedule_window, is_recurring: true, end_date: "") }
 
     describe "Validations" do
       it { should validate_presence_of(:start_time) }
       it { should validate_presence_of(:end_time) }
-      it { should validate_presence_of(:is_recurring) }
+
+      it "should validate presence of start date" do
+        expect(recurring_schedule_window.valid?).to be_falsey
+      end
+
+      it "should validate presence of end date" do
+        expect(recurring_schedule_window2.valid?).to be_falsey
+      end
     end
 
     describe "Associations" do
