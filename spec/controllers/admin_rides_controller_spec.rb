03bca9faa4ec07e8c4a429d7bf8f56ccd12f5a9d
Narayan Rai <narayanrai97@gmail.com>
2019-07-25 16:43:49 +0000

    Add tests for tokens flow on admin ride create

---
+++ spec/controllers/admin_rides_controller_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe AdminRideController, type: :controller do
+  let!(:admin) { create :user }
+  let!(:select_rider) { create :rider, email: "select_rider@example.com" }
+  let!(:valid_tokens) { create_list :token, 5, rider_id: select_rider.id }
+  let!(:pick_up_time) { Time.zone.now + 15.days }
+
+  describe "POST create" do
+    before do
+      sign_in admin
+      @next_token = select_rider.next_valid_token       # Token id `1`
+    end
+
+    let(:test_response) { post :create, params: { ride: { rider_id: select_rider.id,
+                                                          pick_up_time: pick_up_time,
+                                                          start_street: "201 W. Main st.",
+                                                          start_city: "Durham",
+                                                          start_state: "NC",
+                                                          start_zip: "27701",
+                                                          end_street: "1200 Foster st.",
+                                                          end_city: "Durham",
+                                                          end_state: "NC",
+                                                          end_zip: "27705",
+                                                          reason: "Doctor visit",
+                                                          status: "requested"
+                                                        }
+                                                  }
+                            }
+
+    it "changes the ride count and token count on admin ride create" do
+
+      expect do
+          test_response
+          @next_token.ride_id = Ride.last.id
+          @next_token.save
+      end.to change(Ride, :count)
+
+      expect(select_rider.valid_tokens_count).to eq(4) # Token count change after admin ride create
+    end
+
+  end
+end
