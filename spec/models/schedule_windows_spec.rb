59aab036edd77a3ee5b2dd0a4dfacd63a21acc6d
KWasi Yeboah <yeboah02208@gmail.com>
2019-07-29 20:19:40 +0000

    testing model methods

--- spec/models/schedule_windows_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe ScheduleWindow, type: :model do
-  describe "Assocation" do
-     it {should belong_to(:driver) } 
-     it{should belong_to(:location) }
-  end
-end
