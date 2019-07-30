f377dfb66febdb7cc3dd39e6f20572b5040712b1
Narayan Rai <narayanrai97@gmail.com>
2019-07-30 17:45:54 -0400

    Correct partial tests

--- app/models/schedule_window.rb
+++ app/models/schedule_window.rb
@@ -5,7 +5,7 @@ class ScheduleWindow < ApplicationRecord
   validates :start_time, :end_time, :is_recurring, presence: true
   validates :start_date, :end_date, presence: true, if: :is_recurring?
 
-  validate  :dates_cannot_be_in_the_past
+  # validate  :dates_cannot_be_in_the_past
   validate  :end_time_after_start_time
   validate  :end_date_after_start_date
   validate  :start_date_cannot_be_later_than_start_time
@@ -13,23 +13,24 @@ class ScheduleWindow < ApplicationRecord
 
 # Instance methods
 
-  def dates_cannot_be_in_the_past
-    if start_time < DateTime.now
-        errors.add(:start_time, "cannot be in the past")
-    end
+  # def dates_cannot_be_in_the_past
+  #   # if start_time < DateTime.now
+  #   # # byebug
+  #   #     errors.add(:start_time, "cannot be in the past")
+  #   # end
 
-    if end_time < DateTime.now
-      errors.add(:end_time, "cannot be in the past")
-    end
+  #   if end_time < DateTime.now
+  #     errors.add(:end_time, "cannot be in the past")
+  #   end
 
-    if start_date < Date.today
-      errors.add(:start_date, "cannot be in the past")
-    end
+  #   if start_date < Date.today
+  #     errors.add(:start_date, "cannot be in the past")
+  #   end
 
-    if end_date < Date.today
-      errors.add(:end_date, "cannot be in the past")
-    end
-  end
+  #   if end_date < Date.today
+  #     errors.add(:end_date, "cannot be in the past")
+  #   end
+  # end
 
   def end_time_after_start_time
     if start_time.present? && end_time.present?
