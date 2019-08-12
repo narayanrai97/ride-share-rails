124f0385653e2a8cc805f83004c2fd0bc530d162
Alik Lynn Lyle <aliklynnlyle@gmail.com>
2019-08-12 18:12:29 -0400

    took out byebug lines did NOT add pundit code yet

--- app/policies/token_policy.rb
+++ app/policies/token_policy.rb
@@ -2,13 +2,11 @@
 # controls who can access disposal controller actions
 class TokenPolicy < ApplicationPolicy
     def token_belongs_to_org?
-      byebug
       token.organization == current_user.organization
 
     end
-  
+
     %i(show? edit? update? delete?).each do |ali|
-      byebug
-      alias_method ali, :token_or_up?  
+      alias_method ali, :token_or_up?
     end
   end
