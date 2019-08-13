c35c76dac46ce937e29c816a23606ff8ab835546
Narayan Rai <narayanrai97@gmail.com>
2019-08-13 16:38:40 -0400

    Fix merge conflict on admin rides controller create action

--- app/controllers/admin_ride_controller.rb
+++ app/controllers/admin_ride_controller.rb
@@ -45,11 +45,7 @@ class AdminRideController < ApplicationController
                             start_location: start_location,
                             end_location: end_location,
                             reason: ride_params[:reason])
-<<<<<<< HEAD
     @ride.status = "approved" if current_user.organization.use_tokens?
-=======
-    @ride.update_attribute(:status, 'approved') if current_user.organization.use_tokens?
->>>>>>> master
 
     if @ride.save
       token.ride_id = @ride.id
