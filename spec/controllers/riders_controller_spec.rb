8a4313a8a6e9d8703762a5ecc5f097451f0d15a2
Alik Lynn Lyle <aliklynnlyle@gmail.com>
2019-08-27 18:32:28 -0400

    switched to riders controller for tests

---
+++ spec/controllers/riders_controller_spec.rb
@@ -0,0 +1,14 @@
+
+require 'rails_helper'
+require 'spec_helper'
+
+RSpec.describe RidersController, type: :controller do
+  byebug
+  let(:@org1) { create :organization }
+  # let(:org2) { create :organization }
+  # let(:user1){ create :user }
+  # let(:user2){ create :user }
+  # let(:rider1) { create :rider }
+  # let(:rider2) { create :rider }
+  byebug
+end
