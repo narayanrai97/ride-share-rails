52bc0cae7718deb8d55f5e6c1a1ecbbe35da9943
Alik Lynn Lyle <aliklynnlyle@gmail.com>
2019-08-28 16:09:17 -0400

    added additional ride and org to factories and created let statement, before block and first it statement

--- spec/controllers/riders_controller_spec.rb
+++ spec/controllers/riders_controller_spec.rb
@@ -1,14 +1,35 @@
 
 require 'rails_helper'
-require 'spec_helper'
 
 RSpec.describe RidersController, type: :controller do
-  byebug
-  let(:@org1) { create :organization }
-  # let(:org2) { create :organization }
-  # let(:user1){ create :user }
-  # let(:user2){ create :user }
-  # let(:rider1) { create :rider }
-  # let(:rider2) { create :rider }
-  byebug
+  let!(:user1){ create :user }
+
+  before do
+     sign_in user1
+   end
+
+  it 'creates a rider' do
+   expect do
+     test_response = post :create, params: {
+       rider: {
+        first_name: 'John',
+        last_name: 'Doe',
+        phone: '12345',
+        email: 'wasemail@this.com',
+        password: 'password',
+        password_confirmation: 'password'
+        }
+      }
+   expect(test_response.response_code).to eq(302)
+   expect(test_response).to redirect_to(Rider.first)
+   end.to change(Rider, :count)
+ end
+
+  # it 'updates a rider'
+  # it 'fails to update a rider in a different organization than the active user'
+  # it 'bulk updates tokens for a rider'
+  # it 'fails to update tokens for a rider in a different organization than the active user'
+  # it 'deactivates a rider'
+  # it 'fails to deactivate a rider in a different organization than the active user'
+
 end
--- spec/factories/riders.rb
+++ spec/factories/riders.rb
@@ -1,11 +1,19 @@
 FactoryBot.define do
   factory :rider do
-    first_name { "Ubber" }
-    last_name { "Rider"}
-    phone { "919-999-8888" }
-    email { "rider@example.com" }
-    password { "password" }
+    first_name {"Ubber"}
+    last_name {"Rider"}
+    phone {"919-999-8888"}
+    email {"rider@example.com"}
+    password {"password"}
+    association :organization
+  end
 
-      association :organization
+  factory :rider2, class: Rider do
+    first_name {"Lyfted"}
+    last_name {"Rider"}
+    phone {"919-111-2222"}
+    email {"rider2@example.com"}
+    password {"password"}
+    association :organization2
   end
 end
