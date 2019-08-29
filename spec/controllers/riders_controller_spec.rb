f7b95c1494a251f5d50a78b1e7b08f9e4d204c36
Alik Lynn Lyle <aliklynnlyle@gmail.com>
2019-08-29 16:14:11 -0400

    both create and update tests for riders controller now working

--- spec/controllers/riders_controller_spec.rb
+++ spec/controllers/riders_controller_spec.rb
@@ -3,10 +3,11 @@ require 'rails_helper'
 
 RSpec.describe RidersController, type: :controller do
   let!(:user1){ create :user }
+  let!(:rider1){ create :rider, organization_id: user1.organization.id }
 
   before do
      sign_in user1
-   end
+  end
 
   it 'creates a rider' do
    expect do
@@ -20,12 +21,27 @@ RSpec.describe RidersController, type: :controller do
         password_confirmation: 'password'
         }
       }
-   expect(test_response.response_code).to eq(302)
-   expect(test_response).to redirect_to(Rider.first)
-   end.to change(Rider, :count)
- end
+     expect(test_response.response_code).to eq(302)
+     expect(test_response).to redirect_to(Rider.last)
+     end.to change(Rider, :count)
+   end
 
-  # it 'updates a rider'
+  it 'updates a rider' do
+    test_response = put :update, params: {
+      id: Rider.first.id,
+      rider: {
+      first_name: 'Jane'
+      }
+    }
+
+    first_rider = Rider.first
+    expect(first_rider.first_name).to eq("Jane")
+    expect(test_response.response_code).to eq(302)
+    expect(test_response).to redirect_to(Rider.first)
+  end
+
+  # Ramiro, will working on the following next:
+
   # it 'fails to update a rider in a different organization than the active user'
   # it 'bulk updates tokens for a rider'
   # it 'fails to update tokens for a rider in a different organization than the active user'
