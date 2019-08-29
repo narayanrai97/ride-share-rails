4af4d9cac815d2989444f4215ab9899e3077a36b
Alik Lynn Lyle <aliklynnlyle@gmail.com>
2019-08-29 17:52:25 -0400

    dried up riders spec and factory

--- spec/controllers/riders_controller_spec.rb
+++ spec/controllers/riders_controller_spec.rb
@@ -1,51 +1,66 @@
+# frozen_string_literal: true
 
 require 'rails_helper'
 
 RSpec.describe RidersController, type: :controller do
-  let!(:user1){ create :user }
-  let!(:rider1){ create :rider, organization_id: user1.organization.id }
+  let!(:user) { create :user }
+  let!(:rider) { create :rider, organization_id: user.organization.id }
+  let!(:rider_outside_organization) { create :rider, email: 'whatever@gmail.com' }
 
   before do
-     sign_in user1
+    sign_in user
   end
 
   it 'creates a rider' do
-   expect do
-     test_response = post :create, params: {
-       rider: {
-        first_name: 'John',
-        last_name: 'Doe',
-        phone: '12345',
-        email: 'wasemail@this.com',
-        password: 'password',
-        password_confirmation: 'password'
+    expect do
+      test_response = post :create, params: {
+        rider: {
+          first_name: 'John',
+          last_name: 'Doe',
+          phone: '12345',
+          email: 'wasemail@this.com',
+          password: 'password',
+          password_confirmation: 'password'
         }
       }
-     expect(test_response.response_code).to eq(302)
-     expect(test_response).to redirect_to(Rider.last)
-     end.to change(Rider, :count)
-   end
+
+      expect(test_response.response_code).to eq(302)
+      expect(test_response).to redirect_to(Rider.last)
+    end.to change(Rider, :count)
+  end
 
   it 'updates a rider' do
     test_response = put :update, params: {
-      id: Rider.first.id,
+      id: rider.id,
       rider: {
-      first_name: 'Jane'
+        first_name: 'Jane'
       }
     }
 
-    first_rider = Rider.first
-    expect(first_rider.first_name).to eq("Jane")
+    rider.reload
+    expect(rider.first_name).to eq('Jane')
     expect(test_response.response_code).to eq(302)
-    expect(test_response).to redirect_to(Rider.first)
+    expect(test_response).to redirect_to(rider)
   end
 
-  # Ramiro, will working on the following next:
+  it 'fails to update a rider in a different organization than active user' do
+    test_response = put :update, params: {
+      id: rider_outside_organization.id,
+      rider: {
+        first_name: 'Jane'
+      }
+    }
 
-  # it 'fails to update a rider in a different organization than the active user'
+    expect(rider_outside_organization.first_name).to_not eq('Jane')
+    expect(test_response.response_code).to eq(302)
+    # Test that it redirect you to rider index page
+    # Test that the toast message is present about being unauthorized
+  end
+
+  # It statements to be created below:
+
   # it 'bulk updates tokens for a rider'
   # it 'fails to update tokens for a rider in a different organization than the active user'
   # it 'deactivates a rider'
   # it 'fails to deactivate a rider in a different organization than the active user'
-
 end
--- spec/factories/riders.rb
+++ spec/factories/riders.rb
@@ -8,12 +8,14 @@ FactoryBot.define do
     association :organization
   end
 
-  factory :rider2, class: Rider do
-    first_name {"Lyfted"}
-    last_name {"Rider"}
-    phone {"919-111-2222"}
-    email {"rider2@example.com"}
-    password {"password"}
-    association :organization2
-  end
+  # We may not need the following. I may implement the faker gem instead.
+  
+  # factory :rider2, class: Rider do
+  #   first_name {"Lyfted"}
+  #   last_name {"Rider"}
+  #   phone {"919-111-2222"}
+  #   email {"rider2@example.com"}
+  #   password {"password"}
+  #   association :organization2
+  # end
 end
