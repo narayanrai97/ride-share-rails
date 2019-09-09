79cc925c2063d6bc891e327e7e77186f9e594916
KWasi Yeboah <yeboah02208@gmail.com>
2019-09-09 19:36:54 +0000

    Added logic to the get api and added testing for it

--- spec/requests/organizations_spec.rb
+++ spec/requests/organizations_spec.rb
@@ -7,13 +7,11 @@ RSpec.describe Api::V1::Organizations, type: :request do
 
     it 'test output of organization endpoint' do
     get '/api/v1/organizations', headers: {"ACCEPT" => "application/json" }
-
-
       #puts response.body
       #Checkout out that response is what it should be
       #Only testing parts I changed on the second and third object
       parsed_json = JSON.parse(response.body)
-      puts parsed_json
+      expect(response).to have_http_status(201)
       expect(parsed_json['organization'][0]['name']).to eq('Duke')
       expect(parsed_json['organization'][0]['street']).to eq('200 Front Street')
       expect(parsed_json['organization'][0]['city']).to eq('Durham')
@@ -21,7 +19,11 @@ RSpec.describe Api::V1::Organizations, type: :request do
       expect(parsed_json['organization'][0]['zip']).to eq('27708')
       expect(parsed_json['organization'][1]['name']).to eq('Chapel Hill Health')
       expect(parsed_json['organization'][2]['zip']).to eq('27709')
-
     end
-
+    
+     it 'should return a 404 error when there nothing to retrieve' do
+       Organization.destroy_all
+    get '/api/v1/organizations', headers: {"ACCEPT" => "application/json" }
+      expect(response).to have_http_status(404)
+    end
 end
