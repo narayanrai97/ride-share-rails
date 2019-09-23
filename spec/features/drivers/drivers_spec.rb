1f3b76597f1231c901c2eef200621434d0c4ff85
Alik Lynn Lyle <aliklynnlyle@gmail.com>
2019-09-23 15:38:59 -0400

    let statements for drivers feature tests

---
+++ spec/features/drivers/drivers_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+RSpec.feature "Drivers", type: :feature, js: true do
+  let!(:organization) { create :organization}
+  let!(:organization_2) {create :organization, name: 'NC State' }
+  let!(:admin) { create :user }
+  let!(:driver) { create :driver, organization_id: user.organization.id }
+  let!(:driver_outside_organization) { create :driver, email: 'adriver@gmail.com' }
+
+    scenario "show and validate the Drivers page" do
+      visit
+
+    end
+end
+
+# EXAMPLE:
+# require 'rails_helper'
+#
+# RSpec.feature "Messages", type: :feature, js: true do
+#   let!(:message) { create :message, title: "Alert!" }
+#   let!(:admin) { create :user, :admin, email: "admin@example.com", password: "foobar"}
+#   let!(:user_message) { create :message, posted: false, posted_at: Time.now }
+#
+#   scenario "user sees message" do
+#     visit root_path(locale: 'en')
+#     expect(page).to have_text "Alert!"
+#     click_button 'my_x_button'
+#     expect(page).to_not have_text "Alert!"
+#   end
+#
+#   scenario "displays user messages when visitor clicks `bell` icon" do
+#     visit root_path(locale: 'en')
+#     click_button 'my_x_button' #hides message
+#     click_link 'my_icon_link'   #displays user_messages
+#     expect(page).to have_text "Alert History"
+#   end
+#
+#   scenario "displays list of messages" do
+#     signin('admin@example.com', 'foobar')
+#     visit root_path(locale: 'en')
+#     expect(page).to have_text('Messages') #actually wanted to test as a link (`have_link`)
+#     click_link 'my_messages_link' #redirects to message index page
+#     expect(page).to have_text "Message Center"
+#   end
+#
+# end
