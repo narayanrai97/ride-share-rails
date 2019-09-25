600561b4a4b0b6836ae11de7f31e75f84d5e2154
KWasi Yeboah <yeboah02208@gmail.com>
2019-09-25 18:39:30 +0000

    installed geocode config

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,9 +3,9 @@ class Location < ApplicationRecord
   has_many :schedule_windows
   #One location would most likely have only one driver. But logic currently not that
   has_many :drivers, through: :location_relationships
-  geocoded_by :full_address
   validates :street, :city, :state, :zip, presence: true
   validates :zip, length: { is: 5 }, numericality: true
+  geocoded_by :full_address
   after_validation :geocode, if: ->(obj) { obj.full_address.present? && obj.street_changed? }
   after_validation :capitalize_first_letter, :upcase_fields
 
@@ -14,11 +14,6 @@ class Location < ApplicationRecord
     if self.city
       self.city = self.city.split.map(&:capitalize).join(' ')
     end
-  end
-  
-  def full_address
-    sub_address = [address, city, state].compact.join(', ')
-    [sub_address, zipcode].compact.join(' ')
   end
 
   def upcase_fields
