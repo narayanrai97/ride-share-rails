ba4398cae23b5f6afba1d4b902887e5f00387898
Narayan Rai <narayanrai97@gmail.com>
2019-10-06 11:47:08 -0400

    Fix Driver deactivation/reactivaton tests

--- spec/controllers/drivers_controller_spec.rb
+++ spec/controllers/drivers_controller_spec.rb
@@ -42,7 +42,7 @@ RSpec.describe DriversController, type: :controller do
         }
       }
 
-      expect(flash[:notice]).to match(/has been saved/)
+      expect(flash[:notice]).to match("Driver created.")
       expect(test_response.response_code).to eq(302)
       expect(test_response).to redirect_to(Driver.last)
     end.to change(Driver, :count)
@@ -177,25 +177,27 @@ RSpec.describe DriversController, type: :controller do
     expect(test_response).to redirect_to(drivers_path)
   end
 
-  it 'deactivates driver' do
-    test_response = put :deactivate, params: {
+  it 'deactivates and reactivates a driver' do
+    # Deactivation first
+    test_response = put :activation, params: {
       driver_id: driver.id
       }
 
     driver.reload
     expect(driver.is_active).to be(false)
     expect(test_response.response_code).to eq(302)
-    expect(flash[:notice]).to match(/deactivated/)
+    expect(flash[:alert]).to match("Driver deactivated.")
     expect(test_response).to redirect_to(drivers_path)
 
-    test_response_2 = put :deactivate, params: {
+    # Reactivaton
+    test_response_2 = put :activation, params: {
       driver_id: driver.id
     }
 
     driver.reload
     expect(driver.is_active).to be(true)
     expect(test_response_2.response_code).to eq(302)
-    expect(flash[:notice]).to match(/set to active/)
+    expect(flash[:notice]).to match("Driver reactivated.")
     expect(test_response_2).to redirect_to(drivers_path)
   end
 
