dd5f7c1d9259a2a67d77463b8d26184a22773e07
John McGarvey <jmcgarve@bellsouth.net>
2019-10-16 11:00:00 -0400

    fix location relationship with driver and rider

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,7 +2,8 @@ class Location < ApplicationRecord
   has_many :location_relationships
   has_many :schedule_windows
   #One location would most likely have only one driver. But logic currently not that
-  has_many :drivers, through: :location_relationships
+  has_many :drivers,  -> { distinct }, through: :location_relationships
+  has_many :riders, -> { distinct }, through: :location_relationships
   validates :street, :city, :state, :zip, presence: true
   validates :zip, length: { is: 5 }, numericality: true
   geocoded_by :full_address
