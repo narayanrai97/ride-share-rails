b74b3acb3788deeb4b70b00d8657d2a0e0c71fb7
Alik Lynn Lyle <aliklynnlyle@gmail.com>
2019-10-17 18:47:13 -0400

    recommit of working code

--- spec/features/riders/riders_spec.rb
+++ spec/features/riders/riders_spec.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
-
+​
 require 'rails_helper'
-
+​
 RSpec.feature 'Riders', type: :feature, js: true do
   include Features::SessionHelpers
-
+​
   let!(:organization) { create :organization }
   let!(:admin) { create :user }
   let!(:rider) { create :rider, organization_id: admin.organization.id }
@@ -14,7 +14,7 @@ RSpec.feature 'Riders', type: :feature, js: true do
   let!(:ride){create(:ride, rider_id: rider.id, organization_id: admin.organization.id,
     start_location_id: location1.id, end_location_id: location1.id)}
   let!(:location_relationship) { create :location_relationship, rider_id: rider.id, location_id: location.id }
-
+​
   scenario 'when admin selects list of riders, check that all riders are in org 'do
     visit root_path
     click_link 'Login as Admin'
@@ -22,33 +22,25 @@ RSpec.feature 'Riders', type: :feature, js: true do
     fill_in 'Email', with: admin.email
     fill_in 'Password', with: 'password'
     click_on 'Log in'
-<<<<<<< HEAD
     save_screenshot
     expect(page).to have_text admin.email
-=======
-    expect(page).to have_text "Welcome #{admin.email}!"
->>>>>>> master
     click_link 'Riders'
     expect(page).to have_text rider.first_name
     expect(page).not_to have_text rider_outside_organization.first_name
   end
-
+​
   scenario 'when admin clicks show, fields should be populated' do
     visit root_path
     click_link 'Login as Admin'
     expect(page).to have_text 'Log in'
     signin('user@example.com', 'password')
-<<<<<<< HEAD
     expect(page).to have_text admin.email
-=======
-    expect(page).to have_text 'Welcome user@example.com!'
->>>>>>> master
     click_link 'Riders'
     click_link 'Show'
-    expect(page).to have_text 'Rider Information'
-    expect(page).to have_text 'Ubber'
-    expect(page).to have_text '919-999-8888'
-    expect(page).to have_text '800 Park Offices Dr'
-    expect(page).to have_text 'Interview'
+    expect(page).to have_text 'Personal Information'
+    expect(page).to have_text rider.first_name
+    expect(page).to have_text rider.phone
+    expect(page).to have_text location.street
+    expect(page).to have_text ride.reason
   end
 end
