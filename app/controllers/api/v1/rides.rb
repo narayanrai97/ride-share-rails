5e25050df17a2d6c0d79f0f260bd5ca0286eafaa
Narayan Rai <narayanrai97@gmail.com>
2019-10-23 14:39:20 -0400

    Add check that drivers can only accept rides with status 'approved'

--- app/controllers/api/v1/rides.rb
+++ app/controllers/api/v1/rides.rb
@@ -6,170 +6,162 @@ module Api
       helpers SessionHelpers
       helpers RideHelpers
 
-
       before do
         error!('Unauthorized', 401) unless require_login!
       end
 
+      desc "Return all rides"
+      params do
+        optional :start, type: DateTime, desc: "Start date for rides"
+        optional :end, type: String, desc: "End date for rides"
+        optional :status, type: Array[String], desc: "String of status wanted"
+        optional :driver_specific, type: Boolean, desc: "Boolean if rides are driver specific"
+        #Missing functionality for radius feature currently
+        optional :radius, type: Boolean, desc: "Boolean if rides are within radius"
+        optional :location_id, type: Integer, desc: "location to use for radius"
+      end
+      get "rides", root: :rides do
 
-        desc "Return all rides"
-        params do
-          optional :start, type: DateTime, desc: "Start date for rides"
-          optional :end, type: String, desc: "End date for rides"
-          optional :status, type: Array[String], desc: "String of status wanted"
-          optional :driver_specific, type: Boolean, desc: "Boolean if rides are driver specific"
-          #Missing functionality for radius feature currently
-          optional :radius, type: Boolean, desc: "Boolean if rides are within radius"
-          optional :location_id, type: Integer, desc: "location to use for radius" 
+        driver = current_driver
+         if driver.is_active == false
+           status 401
+           return "Not Authorized"
         end
-          get "rides", root: :rides do
-            
-            driver = current_driver
-             if driver.is_active == false
-               status 401
-               return "Not Authorized"
-            end
-            
-            start_time = params[:start]
-            end_time = params[:end]
-            
-            if start_time != nil and end_time != nil
-              rides = Ride.where(organization_id: driver.organization_id).where("pick_up_time >= ?", start_time).where("pick_up_time <= ?", end_time)
-              if rides.length == 0
-                status 404
-                return ""
-              end
-            else 
-              rides = Ride.where(organization_id: driver.organization_id)
-              if rides.length == 0
-                status 404
-                return ""
-              end
-            end
 
-            status = params[:status]
-            # status = Array["pending", "scheduled"]
+        start_time = params[:start]
+        end_time = params[:end]
 
-            if status != nil
-              rides = rides.where(status: status)
-              if rides.length == 0
-                status 404
-                return ""
-              end
-            end
-            if params[:driver_specific] == true
-              rides = rides.where(driver_id: driver.id)
-              if rides.length == 0
-                status 404
-                return ""
-              end
-            end
-
-            # if params[:radius] == true
-            #   rides.each do |ride|
-            #     if check_radius(ride)
-            #   end
-            # end
-            if params[:location_id] != nil 
-              begin 
-                location = Location.find(params[:location_id]) 
-              rescue ActiveRecord::RecordNotFound => e
-                location = nil
-              end
-              if location != nil 
-                if location.latitude != nil && location.longitude != nil
-                  rides_near = rides.select {|ride| ride.is_near?([location.latitude, location.longitude],driver.radius ) }
-                  if rides_near.length > 0
-                    status 200 
-                    return rides_near
-                  else
-                    status 404
-                    return ""
-                  end
-                else 
-                  status 400
-                  return "bad requests"
-                end
-              else 
-                status 400
-                return "bad requested"
-              end
-            end
-            status 200
-            return rides
+        if start_time != nil and end_time != nil
+          rides = Ride.where(organization_id: driver.organization_id).where("pick_up_time >= ?", start_time).where("pick_up_time <= ?", end_time)
+          if rides.length == 0
+            status 404
+            return ""
           end
-
-          desc "Return a ride with given ID"
-          params do
-            requires :ride_id, type: String, desc: "ID of the
-                ride"
+        else
+          rides = Ride.where(organization_id: driver.organization_id)
+          if rides.length == 0
+            status 404
+            return ""
           end
-          
-          get "rides/:ride_id", root: :ride do
-            
-            driver = current_driver
-            if driver.is_active == false
-              status 401
-              return "Not Authorized "
-            end
-            
-            #Only can see rides that the driver own for or have no driver
-            ride = Ride.find(permitted_params[:ride_id])
-            if (ride.driver_id == nil or ride.driver_id ==driver.id)
-              status 201 
-              render ride
+        end
+
+        status = params[:status]
+        # status = Array["pending", "scheduled"]
+
+        if status != nil
+          rides = rides.where(status: status)
+          if rides.length == 0
+            status 404
+            return ""
+          end
+        end
+        if params[:driver_specific] == true
+          rides = rides.where(driver_id: driver.id)
+          if rides.length == 0
+            status 404
+            return ""
+          end
+        end
+
+        # if params[:radius] == true
+        #   rides.each do |ride|
+        #     if check_radius(ride)
+        #   end
+        # end
+        if params[:location_id] != nil
+          begin
+            location = Location.find(params[:location_id])
+          rescue ActiveRecord::RecordNotFound => e
+            location = nil
+          end
+          if location != nil
+            if location.latitude != nil && location.longitude != nil
+              rides_near = rides.select {|ride| ride.is_near?([location.latitude, location.longitude],driver.radius ) }
+              if rides_near.length > 0
+                status 200
+                return rides_near
+              else
+                status 404
+                return ""
+              end
             else
-              status(404)
-              render "Unauthorized"
-            end
-          end
-
-        desc "Accept a ride"
-        params do
-          requires :ride_id, type: String, desc: "ID of the ride"
-        end
-        post "rides/:ride_id/accept" do
-          driver = current_driver
-          if driver.is_active == false
-            status 401
-            return "Not Authorized"
-          end
-            
-          ride = Ride.find(permitted_params[:ride_id])
-          if (ride.driver_id == nil or ride.driver_id == driver.id)
-            if ride.update(driver_id: driver.id, status: "scheduled")
-              status 201
-              render ride
+              status 400
+              return "bad requests"
             end
           else
-            status 401
-            return "Unauthorized"
+            status 400
+            return "bad requested"
           end
         end
+        status 200
+        return rides
+      end
 
+      desc "Return a ride with given ID"
+      params do
+        requires :ride_id, type: String, desc: "ID of the
+            ride"
+      end
 
-        desc "Complete a ride"
-        params do
-          requires :ride_id, type: String, desc: "ID of the ride"
+      get "rides/:ride_id", root: :ride do
+
+        driver = current_driver
+        if driver.is_active == false
+          status 401
+          return "Not Authorized "
         end
-        post "rides/:ride_id/complete" do
-          driver = current_driver
-          if driver.is_active == false
-            status 401
-            return "Not Authorized"
-         end
-          
-          ride = Ride.find(permitted_params[:ride_id])
-          if (ride.driver_id == nil or ride.driver_id ==driver.id)
-            if ride.update(driver_id: driver.id, status: "completed")
-              status 201
-              render ride
-            end
-          else
-             status 401
-             render "Unauthorized"
+
+        #Only can see rides that the driver own for or have no driver
+        ride = Ride.find(permitted_params[:ride_id])
+        if (ride.driver_id == nil or ride.driver_id ==driver.id)
+          status 201
+          render ride
+        else
+          status(404)
+          render "Unauthorized"
+        end
+      end
+
+      # Driver Acceting a ride
+      desc "Accept a ride"
+      params do
+        requires :ride_id, type: String, desc: "ID of the ride"
+      end
+      post "rides/:ride_id/accept" do
+        ride = Ride.find(permitted_params[:ride_id])
+
+        if current_driver.is_active? && ride.driver_id.nil? && ride.status == "approved"
+          ride.update(driver_id: current_driver.id, status: "scheduled")
+          status 201
+          render ride
+        else
+          status 401
+          return "Not Authorized"
+        end
+      end
+
+      desc "Complete a ride"
+      params do
+        requires :ride_id, type: String, desc: "ID of the ride"
+      end
+      post "rides/:ride_id/complete" do
+        driver = current_driver
+        if driver.is_active == false
+          status 401
+          return "Not Authorized"
+       end
+
+        ride = Ride.find(permitted_params[:ride_id])
+        if (ride.driver_id == nil or ride.driver_id ==driver.id)
+          if ride.update(driver_id: driver.id, status: "completed")
+            status 201
+            render ride
           end
+        else
+           status 401
+           render "Unauthorized"
         end
+      end
 
       desc "Cancel a ride"
       params do
@@ -177,7 +169,7 @@ module Api
       end
       post "rides/:ride_id/cancel" do
         driver = current_driver
-        
+
         ride = Ride.find(permitted_params[:ride_id])
         if (ride.driver_id == nil or ride.driver_id ==driver.id)
           if ride.update(driver_id: driver.id, status: "canceled")
@@ -201,7 +193,7 @@ module Api
           status 401
           return "Not Authorized"
         end
-        
+
         ride = Ride.find(permitted_params[:ride_id])
         if (ride.driver_id == nil or ride.driver_id ==driver.id)
           if ride.update(driver_id: driver.id, status: "picking-up")
