ab361e65c6c84da94f6e26204221aaf60a204a8b
KWasi Yeboah <yeboah02208@gmail.com>
2019-11-06 15:26:11 +0000

    Cleaning up code

--- app/controllers/javascript_controller.rb
+++
@@ -1,5 +0,0 @@
-class JavascriptController < ApplicationController
-    def dynamic_locations
-        @location = location.find(:all)
-    end
-end
--- spec/controllers/javascript_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe JavascriptController, type: :controller do
-
-end
