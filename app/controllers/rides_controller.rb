c9683a83e689da5e77b5dc6ce18d6e15412c6b0e
KWasi Yeboah <yeboah02208@gmail.com>
2019-11-21 16:03:18 +0000

    working on ride database

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -47,36 +47,26 @@ class RidesController < ApplicationController
       end
 
       if current_rider.organization.use_tokens == false or token != nil
-        start_location = Location.find_or_create_by(
+        @start_location = Location.new(
           street: ride_params[:start_street],
           city: ride_params[:start_city],
           state: ride_params[:start_state],
           zip: ride_params[:start_zip])
-
-        sl = start_location.save
-        if sl == nil
-          flash[:error] = start_location.errors.full_messages.join(" ")
-          render 'new'
-        end
-
-        end_location = Location.find_or_create_by(
+        save_location_error_handler(@start_location)
+        
+        @end_location = Location.new(
           street: ride_params[:end_street],
           city: ride_params[:end_city],
           state: ride_params[:end_state],
           zip: ride_params[:end_zip])
-
-        el = end_location.save
-        if el == nil
-          flash[:error] = end_location.errors.full_messages.join(" ")
-          render 'new'
-        end
-
+        save_location_error_handler(@end_location)
+        
         @ride = Ride.find_or_create_by(
           rider_id: current_rider.id,
           organization_id: current_rider.organization.id,
           pick_up_time: ride_params[:pick_up_time],
-          start_location_id: start_location.id,
-          end_location_id: end_location.id,
+          start_location_id: @start_location.id,
+          end_location_id: @end_location.id,
           reason: ride_params[:reason])
         if current_rider.organization.use_tokens?
           @ride.status = "approved"
@@ -84,25 +74,9 @@ class RidesController < ApplicationController
           @ride.status = "pending"
         end
         if @ride.save
-
-          if ride_params[:save_start_location]
-            LocationRelationship.create(location_id: start_location.id, rider_id: current_rider.id)
-          end
-
-          if ride_params[:save_end_location]
-            LocationRelationship.create(location_id: end_location.id, rider_id: current_rider.id)
-          end
-
-          if ride_params[:save_start_location] == true or ride_params[:save_end_location] == true
-            rider_location = location_relationships.order(update_at: :desc)
-            if (rider_location.count > 15)
-              for i in (15..rider_location.count-1) do
-                rider_location[i].destroy
-              end
-            end
-          end
-
-
+        rider_choose_save_location
+        only_15_location_saves
+        
           if token != nil
             token.ride_id = @ride.id
             token.save
@@ -185,6 +159,33 @@ class RidesController < ApplicationController
        :save_start_location, :start_street, :start_city, :start_state, :start_zip,
        :save_end_location, :end_street, :end_city, :end_state, :end_zip, :reason, :status)
     end
+    
+    def save_location_error_handler(location)
+        if !location.save
+          flash[:error] = @start_location.errors.full_messages.join(" ")
+          render 'new'
+        end
+    end
+    
+    def rider_choose_save_location
+      if ride_params[:save_start_location]
+        LocationRelationship.create(location_id: @start_location.id, rider_id: current_rider.id)
+      end
+      if ride_params[:save_end_location]
+        LocationRelationship.create(location_id: @end_location.id, rider_id: current_rider.id)
+      end
+    end
+    
+    def only_15_location_saves
+      if ride_params[:save_start_location] == true or ride_params[:save_end_location] == true
+         rider_location = location_relationships.order(update_at: :desc)
+            if (rider_location.count > 15)
+              for i in (15..rider_location.count-1) do
+                rider_location[i].destroy
+              end
+            end
+          end 
+        end
 
     def rider_not_authorized
       flash.notice = "You are not authorized to view this information"
