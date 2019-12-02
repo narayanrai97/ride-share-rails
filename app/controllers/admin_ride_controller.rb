11ca12987e28ae8b6c1010f2a6d5e8cc01935a97
KWasi Yeboah <yeboah02208@gmail.com>
2019-12-02 21:47:47 +0000

    simplify code

--- app/controllers/admin_ride_controller.rb
+++ app/controllers/admin_ride_controller.rb
@@ -162,8 +162,7 @@ class AdminRideController < ApplicationController
 
   # TODO: -- possibly clean out old record, and make a plan to fix it in the future.
   def save_location_error_handler(location)
-    location_validate = location.validate
-    if !location_validate 
+    if !location.validate 
       flash.now[:alert] = location.errors.full_messages.join("\n")
       @ride = Ride.new
       render "new"
@@ -185,8 +184,7 @@ class AdminRideController < ApplicationController
   end
 
   def update_location_error_handler(location)
-    location_validate = location.validate
-    if !location_validate
+    if !location.validate
       flash.now[:alert] = location.errors.full_messages.join("\n")
       @ride = Ride.find(params[:id])
       render "edit"
