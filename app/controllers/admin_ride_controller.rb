efd68afd52604af71b0b6aad814884a8ed769890
KWasi Yeboah <yeboah02208@gmail.com>
2019-12-05 17:27:09 +0000

    working on form keeping it data

--- app/controllers/admin_ride_controller.rb
+++ app/controllers/admin_ride_controller.rb
@@ -62,36 +62,33 @@ class AdminRideController < ApplicationController
                                    city: ride_params[:start_city],
                                    state: ride_params[:start_state],
                                    zip: ride_params[:start_zip])
+    @end_location = Location.new(street: ride_params[:end_street],
+                                 city: ride_params[:end_city],
+                                 state: ride_params[:end_state],
+                                 zip: ride_params[:end_zip])
+     @ride = Ride.new(organization_id: current_user.organization_id,
+                     rider_id: rider.id,
+                     pick_up_time: ride_params[:pick_up_time],
+                     reason: ride_params[:reason])
     location = save_location_error_handler(@start_location)
     if location.nil?
       flash.now[:alert] = @start_location.errors.full_messages.join("\n")
-      @ride = Ride.new
       render "new"
       return
     else
       @start_location = location
     end
 
-    @end_location = Location.new(street: ride_params[:end_street],
-                                 city: ride_params[:end_city],
-                                 state: ride_params[:end_state],
-                                 zip: ride_params[:end_zip])
     location = save_location_error_handler(@end_location)
     if location.nil?
       flash.now[:alert] = @end_location.errors.full_messages.join("\n")
-      @ride = Ride.new
       render "new"
       return 
     else
       @end_location = location
     end
-
-    @ride = Ride.new(organization_id: current_user.organization_id,
-                     rider_id: rider.id,
-                     pick_up_time: ride_params[:pick_up_time],
-                     start_location_id: @start_location.id,
-                     end_location_id: @end_location.id,
-                     reason: ride_params[:reason])
+    @ride.start_location_id = @start_location.id
+    @ride.end_location_id = @end_location.id
     @ride.status = (organization.use_tokens ? 'approved' : 'pending')
     if @ride.save
       rider_choose_save_location
@@ -104,7 +101,6 @@ class AdminRideController < ApplicationController
       redirect_to admin_ride_path(@ride)
     else
       flash.now[:alert] = @ride.errors.full_messages.join("\n")
-      @ride = Ride.new
       render 'new'
     end
   end
