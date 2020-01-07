fa201e1aae0265837963606447cd977a204857b6
kwasi008 <Yeboah02208@gmail.com>
2020-01-07 10:25:10 -0500

    Added logic when organzation does not uses token

--- app/controllers/admin_ride_controller.rb
+++ app/controllers/admin_ride_controller.rb
@@ -168,10 +168,16 @@ class AdminRideController < ApplicationController
 
   def cancel
     @ride = Ride.find(params[:id])
+    organization = Organization.find(current_user.organization_id)
     authorize @ride
     if %w[pending approved scheduled].include? @ride.status
       @ride.update_attributes(status: 'canceled')
-      @ride.token.update_attribute(:ride_id, nil)
+
+      # @ride.token.update_attributes(:ride_id, nil)
+      if organization.use_tokens == true
+        token.ride_id = @ride.id
+        token.save
+      end
       flash.notice = 'Ride canceled.'
       redirect_to request.referrer || admin_ride_index_path
     end
@@ -183,7 +189,7 @@ class AdminRideController < ApplicationController
     params.require(:ride).permit(:rider_id, :driver_id, :pick_up_time, :save_start_location,
                                  :save_end_location, :organization_rider_start_location, :start_street, :start_city,
                                  :start_state, :start_zip, :organization_rider_end_location,
-                                 :end_street, :end_city, :end_state, :end_zip, :reason, :status, :q)
+                                 :end_street, :end_city, :end_state, :end_zip, :reason, :status, :quary)
   end
 
   # TODO: -- possibly clean out old record, and make a plan to fix it in the future.
