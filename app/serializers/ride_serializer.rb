4330a843389ed96f7dd3d606a56bf077a0c0cd5e
Narayan Rai <narayanrai97@gmail.com>
2020-02-10 16:45:44 -0500

    Add 'round_trip' and 'expected_wait_time' on ride serializer

--- app/serializers/ride_serializer.rb
+++ app/serializers/ride_serializer.rb
@@ -1,5 +1,5 @@
 class RideSerializer < ActiveModel::Serializer
   ActiveModelSerializers.config.adapter = :json
-  attributes :id, :organization_id, :rider_id, :driver_id, :pick_up_time, :start_location, :end_location,  :reason, :status
+  attributes :id, :organization_id, :rider_id, :driver_id, :pick_up_time, :start_location, :end_location,  :reason, :status, :round_trip, :expected_wait_time
 
 end
