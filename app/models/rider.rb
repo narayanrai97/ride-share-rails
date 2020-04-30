fbd3981dfd046f0d83575656fe1f8d5a2eba9e4c
Narayan Rai <narayanrai97@gmail.com>
2020-04-30 16:44:24 -0400

    Add password validations on Driver, User, and Rider models

--- app/models/rider.rb
+++ app/models/rider.rb
@@ -10,6 +10,7 @@ class Rider < ApplicationRecord
 
   validates :first_name, :last_name, :phone, :organization, presence: true
   validates :phone, length: { is: 10 }, numericality: true
+  validates :password, format: {with: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,25}$/, multiline: true, message: 'must be at least 8 characters long and include 1 uppercase, 1 number, and 1 special character.'}
   scope :active, -> { where(is_active: true) }
   before_save :lets_cap, only: [:new, :create]
 
