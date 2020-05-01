e1bf6a97fed25fa10cd4bba6b725ff73e6e52ba1
Narayan Rai <narayanrai97@gmail.com>
2020-05-01 18:26:56 -0400

    Change password validation method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
-  validates :password, format: {with: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,25}$/, multiline: true, message: 'must be at least 8 characters long and include 1 uppercase, 1 number, and 1 special character.'}
+  validate :password_complexity
 
   belongs_to :organization
+
+  def password_complexity
+    if password.present? and not password.match(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,25}$/)
+      errors.add :password, "must be at least 8 characters long and include 1 uppercase, 1 number, and 1 special character."
+    end
+  end
 end
