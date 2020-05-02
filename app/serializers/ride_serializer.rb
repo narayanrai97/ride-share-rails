fef5ec002537f5f794da7a0972eeeefd1887a392
Narayan Rai <narayanrai97@gmail.com>
2020-05-01 22:38:00 -0400

    Rename ride's field names

--- app/serializers/ride_serializer.rb
+++ app/serializers/ride_serializer.rb
@@ -1,6 +1,6 @@
 class RideSerializer < ActiveModel::Serializer
   ActiveModelSerializers.config.adapter = :json
   attributes :id, :organization_id, :rider_id, :driver_id, :pick_up_time, :start_location, :end_location,  :reason, :status, :completed_at, :round_trip, :expected_wait_time,
-  :pick_up_to_drop_off_distance, :pick_up_to_drop_off_time
+  :pickup_to_dropoff_distance, :pickup_to_dropoff_time
 
 end
