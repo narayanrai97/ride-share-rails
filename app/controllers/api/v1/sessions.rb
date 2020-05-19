08f4595bf13231db7c50f79fe1c18bcad8dd450a
Narayan Rai <narayanrai97@gmail.com>
2020-05-19 16:14:02 -0400

    Add endpoint description and parameters

--- app/controllers/api/v1/sessions.rb
+++ app/controllers/api/v1/sessions.rb
@@ -6,28 +6,31 @@ module Api
       helpers SessionHelpers
 
 
-      desc "Login and return a token on successful login"
+      desc "Login and return a `token` on successful login"
       params do
         requires :email, type: String, desc: "Email of account"
         requires :password, type: String, desc: "Password of account"
       end
-
-      # Initial login endpoint
       post "login", root: :driver do
         create_token(params[:email], params[:password])
       end
 
-      # Login endpoint with drivers active and approval
+      desc "Return a `token` along with `active` and `approval` flags on successful login"
+      params do
+        requires :email, type: String, desc: "Email of account"
+        requires :password, type: String, desc: "Password of account"
+      end
       post "login1", root: :driver do
         token = create_token(params[:email], params[:password])
-        approved = current_driver.background_check && current_driver.application_state == 'accepted'
-        if current_driver.is_active
+        driver = Driver.find_by_email(params[:email])
+        token[:json][:approved] = driver.background_check && driver.application_state == 'accepted'
+        if driver.is_active
           active = true
         else
-          active = false
+          active = true
         end
-
-        {token: token, approved: approved, active: active}
+        token[:json][:active] = active
+        token
       end
 
       desc "Destroys token"
