13731a07adee62cba44c9ac7af93d9f0a0bff531
Narayan Rai <narayanrai97@gmail.com>
2020-05-19 13:39:13 -0400

    Add 'active' and 'approval' flags on driver login

--- app/controllers/api/v1/sessions.rb
+++ app/controllers/api/v1/sessions.rb
@@ -11,8 +11,23 @@ module Api
         requires :email, type: String, desc: "Email of account"
         requires :password, type: String, desc: "Password of account"
       end
+
+      # Initial login endpoint
       post "login", root: :driver do
         create_token(params[:email], params[:password])
+      end
+
+      # Login endpoint with drivers active and approval
+      post "login1", root: :driver do
+        token = create_token(params[:email], params[:password])
+        approved = current_driver.background_check && current_driver.application_state == 'accepted'
+        if current_driver.is_active
+          active = true
+        else
+          active = false
+        end
+
+        {token: token, approved: approved, active: active}
       end
 
       desc "Destroys token"
