c24fc5072fb6e2eb0b37a6fb93064e37e6d2a3d9
kwasi008 <Yeboah02208@gmail.com>
2020-06-11 15:32:34 -0400

    Added rspec test for post and get

--- spec/requests/locations_spec.rb
+++ spec/requests/locations_spec.rb
@@ -20,117 +20,183 @@ RSpec.describe Api::V1::Locations, type: :request do
 
 
     #Created a Location based on the logged in user. Uses token previously created
-  it 'will create a location related to logged in user' do
+  context " Create function test " do
+    it 'will create a location related to logged in user' do
+      post '/api/v1/locations', headers: {"ACCEPT" => "application/json",  "Token" => "1234"},
+        params: { location:
+                  { street:"200 W Front St",
+                    city:"Burlington",
+                    state:"NC",
+                    zip: "27215"},
+                  default_location: {
+                    default: true
+                    }}
+        expect(response).to have_http_status(201)
+        parsed_json = JSON.parse(response.body)
+        expect(parsed_json['location']['street']).to eq('200 W Front St')
+        expect(parsed_json['location']['city']).to eq("Burlington")
+        expect(parsed_json['location']['state']).to eq('NC')
+        expect(parsed_json['location']['zip']).to eq( "27215")
+        expect(parsed_json['location']['default_location']).to eq(true)
+    end
 
-    post '/api/v1/locations', headers: {"ACCEPT" => "application/json",  "Token" => "1234"},
-      params: { location:
-                { street:"200 W Front St",
-                  city:"Burlington",
-                  state:"NC",
-                  zip: "27215"},
-                default_location: {
-                  default: true
-                  }}
-      expect(response).to have_http_status(201)
-      parsed_json = JSON.parse(response.body)
-      expect(parsed_json['location']['street']).to eq('200 W Front St')
-      expect(parsed_json['location']['city']).to eq("Burlington")
-      expect(parsed_json['location']['state']).to eq('NC')
-      expect(parsed_json['location']['zip']).to eq( "27215")
-      expect(parsed_json['location']['default_location']).to eq(true)
-  end
-
-  # test should return a error message. Zip code must be numbers
-  it 'should return a error code. Zip code must be number' do
-    post '/api/v1/locations', headers: {"ACCEPT" => "application/json",  "Token" => "1234"},
-      params: {location:{street:"500 Front Street",
-      city:"Burlington",
-      state:"NC",
-      zip: "call me"}}
-
-      expect(response).to have_http_status(400)
-  end
-
-  it 'will return locations of driver ' do
-
-    get '/api/v1/locations', headers: {"ACCEPT" => "application/json",  "Token" => "1234"}
-      parsed_json = JSON.parse(response.body)
-      locations = parsed_json['locations']
-      thislocation = nil
-      locations.each do |loc|
-      if location['street'] == '1200 front st.'
-       thislocation = location
-        break
-       end
-      end
-      expect(locations.length).to eq(2)
-      expect(response).to have_http_status(200)
-  end
-
-  #Give the api a location id and get back information for it
-  #Needs to verify user owns location
-  it 'will return a location based on id passed' do
-
-    get "/api/v1/locations/#{location.id}", headers: {"ACCEPT" => "application/json",  "Token" => "1234"}
-
-      parsed_json = JSON.parse(response.body)
-      expect(parsed_json['location']['street']).to eq('800 Park Offices Dr')
-      expect(parsed_json['location']['city']).to eq('Morrisville')
-      expect(parsed_json['location']['state']).to eq('NC')
-      expect(parsed_json['location']['zip']).to eq( "27560")
-      expect(response).to have_http_status(200)
-  end
-
-  it 'Updates a location with bad values should return an error message.' do
-    put "/api/v1/locations/#{location.id}", headers: {"ACCEPT" => "application/json",  "Token" => "1234"},
-      params: {location:{street:"2210 Front Street",
-      city:"Burlington",
-      state:"NC",
-      zip: "Rails rules!"}}
-
-      expect(response).to have_http_status(400)
-      #uncomment these to see the error messages
-      # parsed_json = JSON.parse(response.body)
-      # puts parsed_json
-  end
-
-  it 'returns a new address when a location is shared by two drivers but update by one driver' do
-    location_relationship = LocationRelationship.where(location: location2.id).first
-    # location_relationship.update(default: params[:default_location][:default], location: new_location)
-      put "/api/v1/locations/#{location2.id}", headers: {"ACCEPT" => "application/json",  "Token" => "5678" },
-      params: { location: { street:"1052 Canal St",
-                            city:"Durham",
-                            state:"NC",
-                            zip: "27701"
-                          },
-        location_relationship: {
-          default: true,
-          location: location.id
+    # test should return a error message. Zip code must be numbers
+    it 'should return a error code 400' do
+      post '/api/v1/locations', headers: {"ACCEPT" => "application/json",  "Token" => "1234"},
+        params: {location:{street:"500 Front Street",
+        city:"Burlington",
+        state:"NC",
+        zip: "call me"}}
+        expect(response).to have_http_status(400)
+    end
+    #Create a new location with with a locationrelationship to the driver
+    it "Create a new location with a default false" do
+      post '/api/v1/locations', headers: {"ACCEPT" => "application/json",  "Token" => "1234"},
+      params: {location:{ street: "3202 W Friendly Ave",
+        city: "Greensboro",
+        state: "NC",
+        zip: "27408"},
+        default_location: {
+          default: false
         }}
+        expect(response).to have_http_status(201)
+        parsed_json = JSON.parse(response.body)
+        expect(parsed_json['location']['street']).to eq('3202 W Friendly Ave')
+        expect(parsed_json['location']['city']).to eq("Greensboro")
+        expect(parsed_json['location']['state']).to eq('NC')
+        expect(parsed_json['location']['zip']).to eq( "27408")
+        expect(parsed_json['location']['default_location']).to eq(false)
+        puts parsed_json
+    end
 
-      expect(response).to have_http_status(200)
-      #uncomment these to see the error messages
-      # parsed_json = JSON.parse(response.body)
-      # puts parsed_json
+    it "Return 400 when default_location is missing" do
+      post '/api/v1/locations', headers: {"ACCEPT" => "application/json",  "Token" => "1234"},
+      params: {location:{ street: "3202 W Friendly Ave",
+        city: "Greensboro",
+        state: "NC",
+        zip: "27408"}}
+        expect(response).to have_http_status(400)
+        parsed_json = JSON.parse(response.body)
+        puts parsed_json
+    end
+
+    it "Return 400 when location is unfound" do
+      post '/api/v1/locations', headers: {"ACCEPT" => "application/json",  "Token" => "1234"},
+      params: {location:{ street: "Jump",
+        city: "Greensboro",
+        state: "NY",
+        zip: "27408"},
+        default_location: {
+          default: false
+        }}
+        expect(response).to have_http_status(400)
+        parsed_json = JSON.parse(response.body)
+        puts parsed_json
+    end
+
+    it "Return 400 when location is nil" do
+      post '/api/v1/locations', headers: {"ACCEPT" => "application/json",  "Token" => "1234"},
+      params: {location: {},
+        default_location: {
+          default: false
+        }}
+        expect(response).to have_http_status(400)
+        parsed_json = JSON.parse(response.body)
+        puts parsed_json
+    end
   end
 
+  context "Return error code 404 when location is nil" do
 
-   #Delete Record based on id
-  it 'will delete a location based on id passed ' do
+    it 'will return locations of driver ' do
+      get '/api/v1/locations', headers: {"ACCEPT" => "application/json",  "Token" => "1234"}
+        parsed_json = JSON.parse(response.body)
+        locations = parsed_json['locations']
+        thislocation = nil
+        locations.each do |loc|
+        if location['street'] == '1200 front st.'
+         thislocation = location
+          break
+         end
+        end
+        expect(locations.length).to eq(2)
+        expect(response).to have_http_status(200)
+    end
 
-    delete "/api/v1/locations/#{location.id}", headers: {"ACCEPT" => "application/json",  "Token" => "1234"}
-      expect(Location.count).to eq(1)
-      expect(response).to have_http_status(200)
+    #Give the api a location id and get back information for it
+    #Needs to verify user owns location
+    it 'will return a location based on id passed' do
+      get "/api/v1/locations/#{location.id}", headers: {"ACCEPT" => "application/json",  "Token" => "1234"}
+        parsed_json = JSON.parse(response.body)
+        expect(parsed_json['location']['street']).to eq('800 Park Offices Dr')
+        expect(parsed_json['location']['city']).to eq('Morrisville')
+        expect(parsed_json['location']['state']).to eq('NC')
+        expect(parsed_json['location']['zip']).to eq( "27560")
+        expect(response).to have_http_status(200)
+    end
+
+    it 'return error code 404 when location can not be found' do
+      get "/api/v1/locations/#{99999}", headers: {"ACCEPT" => "application/json",  "Token" => "1234"}
+        parsed_json = JSON.parse(response.body)
+        expect(response).to have_http_status(404)
+        parsed_json = JSON.parse(response.body)
+        puts parsed_json
+    end
   end
 
-  it 'will return a error code of 401 when location does not belong to a driver ' do
-    delete "/api/v1/locations/#{location.id}", headers: {"ACCEPT" => "application/json",  "Token" => "5678"}
-      expect(response).to have_http_status(401)
+  context " Update function Test" do
+    it 'Updates a location with bad values should return an error message.' do
+      put "/api/v1/locations/#{location.id}", headers: {"ACCEPT" => "application/json",  "Token" => "1234"},
+        params: {location:{street:"2210 Front Street",
+        city:"Burlington",
+        state:"NC",
+        zip: "Rails rules!"}}
+
+        expect(response).to have_http_status(400)
+        #uncomment these to see the error messages
+        # parsed_json = JSON.parse(response.body)
+        # puts parsed_json
+    end
+
+    it 'returns a new address when a location is shared by two drivers but update by one driver' do
+      location_relationship = LocationRelationship.where(location: location2.id).first
+      # location_relationship.update(default: params[:default_location][:default], location: new_location)
+        put "/api/v1/locations/#{location2.id}", headers: {"ACCEPT" => "application/json",  "Token" => "5678" },
+        params: { location: { street:"1052 Canal St",
+                              city:"Durham",
+                              state:"NC",
+                              zip: "27701"
+                            },
+          location_relationship: {
+            default: true,
+            location: location.id
+          }}
+
+        expect(response).to have_http_status(200)
+        #uncomment these to see the error messages
+        # parsed_json = JSON.parse(response.body)
+        # puts parsed_json
+    end
   end
 
-  it 'will return a error of code 404 when location is nil ' do
-    delete "/api/v1/locations/#{44444}", headers: {"ACCEPT" => "application/json",  "Token" => "5678"}
-      expect(response).to have_http_status(404)
+  context "Delete funition Test" do
+     #Delete Record based on id
+    it 'will delete a location based on id passed ' do
+
+      delete "/api/v1/locations/#{location.id}", headers: {"ACCEPT" => "application/json",  "Token" => "1234"}
+        expect(Location.count).to eq(1)
+        expect(response).to have_http_status(200)
+    end
+
+    it 'Return a error code of 401 when location does not belong to a driver ' do
+      delete "/api/v1/locations/#{location.id}", headers: {"ACCEPT" => "application/json",  "Token" => "5678"}
+        expect(response).to have_http_status(401)
+    end
+
+    it 'will return a error of code 404 when location is nil ' do
+      delete "/api/v1/locations/#{44444}", headers: {"ACCEPT" => "application/json",  "Token" => "5678"}
+        expect(response).to have_http_status(404)
+    end
   end
 
 end
