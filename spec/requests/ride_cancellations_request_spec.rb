c06675a886b017d05807b588dd91f68892c26838
Narayan Rai <narayanrai97@gmail.com>
2020-12-17 21:36:43 -0500

    Fix ride cancellations request tests

--- spec/requests/ride_cancellations_request_spec.rb
+++ spec/requests/ride_cancellations_request_spec.rb
@@ -3,8 +3,11 @@ require 'rails_helper'
 RSpec.describe "RideCancellations", type: :request do
 
   describe "GET /show" do
+    let!(:organization) {create :organization}
+    let!(:ride_category) {create :ride_category, organization_id: organization.id}
+    let!(:ride) {create :ride2, reasons_attributes: [details: "Interview", ride_category_id: ride_category.id]}
     it "returns http success" do
-      get "/ride_cancellations/show"
+      get "/admin_ride/#{ride.id}/cancel"
       expect(response).to have_http_status(:success)
     end
   end
