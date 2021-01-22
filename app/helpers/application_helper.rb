455e43d199ea0b657bc2b53e0e4f4ee9fd625711
kwasi008 <Yeboah02208@gmail.com>
2021-01-22 14:39:47 -0500

    changed how the flash messages werw working

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,20 @@ module ApplicationHelper
   def driver_active_inactive(is_active)
     is_active ? "Yes" : "No"
   end
-  
+
+  def bootstrap_class_for(flash_type)
+  case flash_type
+  when "notice"
+      "alert-success"
+  when "error"
+    "alert-danger"
+  when "alert"
+    "alert-info"
+  else
+    flash_type.to_s
+    end
+  end
+
   # def javascript(*files)
   # content_for(:head) { javascript_include_tag(*files) }
   # end
