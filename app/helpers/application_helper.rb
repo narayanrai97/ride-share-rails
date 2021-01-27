bca8210003392c2b5206f09a89b35d9d0ae8f9af
kwasi008 <Yeboah02208@gmail.com>
2021-01-27 15:26:03 -0500

    fix indentation in application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,15 +9,15 @@ module ApplicationHelper
   end
 
   def bootstrap_class_for(flash_type)
-  case flash_type
-  when "notice"
+    case flash_type
+    when "notice"
       "alert-success"
-  when "error"
-    "alert-danger"
-  when "alert"
-    "alert-info"
-  else
-    flash_type.to_s
+    when "error"
+      "alert-danger"
+    when "alert"
+      "alert-info"
+    else
+      flash_type.to_s
     end
   end
 
