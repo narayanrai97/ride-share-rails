bbba6a4c2e3e54f0a5ba677aafae390879923061
Narayan Rai <narayanrai97@gmail.com>
2021-03-15 11:26:13 -0400

    Add back accidentally deleted code

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -16,7 +16,7 @@ class Ride < ApplicationRecord
   validates :start_location, :end_location, :pick_up_time, :reason, :status, presence: true
   validate :pick_up_time_cannot_be_in_the_past
   # validate :valid_locations
-  validates :status, inclusion: { in: %w(pending approved scheduled picking-up dropping-off waiting return-picking-up return-dropping-off completed canceled),
+  validates :status, inclusion: { in: %w(pending approved scheduled picking-up dropping-off returning-home waiting return-picking-up return-dropping-off completed canceled),
   message: "%{value} is not a valid status" }
   # validates :expected_wait_time, presence: true, if: :round_trip?
   after_validation :set_distance, on: [ :create, :update ]
